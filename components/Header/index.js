0315acad7feca2ea2630ab4d98a49d3aabd8e5e5
Kamal Arieff <kamal.arieff@mudah.my>
2021-01-11 21:42:05 +0800

    feat: use tailwind for drawer menu

--- components/Header/index.js
+++ components/Header/index.js
@@ -15,7 +15,73 @@ import {
 import { List } from "phosphor-react";
 
 export default function Header() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  // const { isOpen, onOpen, onClose } = useDisclosure();
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <>
+      <div className="md:hidden">
+        {/* There is some padding on the svg. So need to remove by 1 for the margin */}
+        <div className="ml-4 py-4">
+          <List size={36} onClick={() => setOpen(!open)}>
+            Open
+          </List>
+        </div>
+        <div
+          css={[
+            tw`h-full absolute top-0 transition flex`,
+            css`
+              background-color: ${open ? "rgba(0, 0, 0, 0.48)" : "unset"};
+            `,
+            open ? tw`w-full` : tw`w-0`,
+          ]}
+          onClick={() => setOpen(false)}
+        >
+          <div
+            css={[
+              tw`w-72 bg-blue-200 transform transition-transform z-50`,
+              open ? tw`translate-x-0` : tw`-translate-x-full`,
+            ]}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <nav className="mt-10 px-6 w-full -ml-5">
+              <a
+                className="flex items-center p-2 my-6 transition-colors duration-200 text-gray-600 rounded-lg "
+                href="#"
+              >
+                <span className="mx-4 text-lg font-normal">Element</span>
+              </a>
+            </nav>
+          </div>
+        </div>
+      </div>
+      {/* Desktop header start */}
+      <h2 tw="max-w-2xl mx-auto text-xl font-bold tracking-tight leading-tight pb-20 pt-8 px-5 cursor-pointer hidden md:(text-4xl tracking-tighter px-0 block)">
+        <div
+          css={css`
+            & > a:hover {
+              ${tw`underline`}
+            }
+          `}
+          tw="flex space-x-8"
+        >
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/">
+            <a>Projects</a>
+          </Link>
+          <Link href="/blog">
+            <a>Blog</a>
+          </Link>
+          <Link href="/profile">
+            <a>About Me</a>
+          </Link>
+        </div>
+      </h2>
+      {/* Desktop header end */}
+    </>
+  );
 
   return (
     <>
