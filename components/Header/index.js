4de7866b357ca075bea63443553c33d3e70e4acf
Kamal Arieff <kamal.arieff@mudah.my>
2021-01-11 22:45:09 +0800

    refactor: remove commented code

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,13 +9,11 @@ import {
   DrawerOverlay,
   DrawerContent,
   DrawerCloseButton,
-  useDisclosure,
   IconButton,
 } from "@chakra-ui/react";
 import { List, X } from "phosphor-react";
 
 export default function Header() {
-  // const { isOpen, onOpen, onClose } = useDisclosure();
   const [open, setOpen] = React.useState(false);
 
   return (
@@ -66,64 +64,6 @@ export default function Header() {
           </div>
         </div>
       </div>
-      {/* Desktop header start */}
-      <h2 tw="max-w-2xl mx-auto text-xl font-bold tracking-tight leading-tight pb-20 pt-8 px-5 cursor-pointer hidden md:(text-4xl tracking-tighter px-0 block)">
-        <div
-          css={css`
-            & > a:hover {
-              ${tw`underline`}
-            }
-          `}
-          tw="flex space-x-8"
-        >
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-          <Link href="/">
-            <a>Projects</a>
-          </Link>
-          <Link href="/blog">
-            <a>Blog</a>
-          </Link>
-          <Link href="/profile">
-            <a>About Me</a>
-          </Link>
-        </div>
-      </h2>
-      {/* Desktop header end */}
-    </>
-  );
-
-  return (
-    <>
-      {/* Drawer menu start */}
-      <div tw="pt-8 md:hidden">
-        <IconButton onClick={onOpen} icon={<List />} />
-        <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
-          <DrawerOverlay>
-            <DrawerContent>
-              <DrawerCloseButton />
-              <DrawerBody>
-                <div tw="flex flex-col space-y-4 pt-8 text-xl font-bold">
-                  <Link href="/">
-                    <a>Home</a>
-                  </Link>
-                  <Link href="/">
-                    <a>Projects</a>
-                  </Link>
-                  <Link href="/blog">
-                    <a>Blog</a>
-                  </Link>
-                  <Link href="/profile">
-                    <a>About Me</a>
-                  </Link>
-                </div>
-              </DrawerBody>
-            </DrawerContent>
-          </DrawerOverlay>
-        </Drawer>
-      </div>
-      {/* Drawer menu end */}
       {/* Desktop header start */}
       <h2 tw="max-w-2xl mx-auto text-xl font-bold tracking-tight leading-tight pb-20 pt-8 px-5 cursor-pointer hidden md:(text-4xl tracking-tighter px-0 block)">
         <div
